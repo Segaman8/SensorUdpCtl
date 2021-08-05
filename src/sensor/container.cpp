02f629dd6085b0b5ef48ac049facd11639cc1e35
Segaman68k <segaman68k@gmail.com>
2021-08-05 17:01:05 +0300

    added emplace

--- src/sensor/container.cpp
+++ src/sensor/container.cpp
@@ -132,13 +132,15 @@ int Container::getKeys (const int **dest)
 
 Container &Container::operator+= (const Unit &u)
 {
-  m_items[u.id()] = u;
+  m_items.emplace (u.id(), u);
+//  m_items[u.id()] = u;
   return *this;
 }
 
 Container &Container::operator+= (Unit &&u)
 {
-  m_items[u.id()] = u;
+  m_items.emplace (u.id(), u);
+//  m_items[u.id()] = u;
   return *this;
 }
 
